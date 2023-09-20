d29c6fd2f355c61dfe1b194034666a2b7e8a9d10
Luis <baskerbyte@gmail.com>
2023-09-20 03:41:54 -0700

    feat: add inputs

---
+++ src/main.py
@@ -0,0 +1,9 @@
+premise = ""
+premises = []
+
+while premise != "0":
+    premise = input("Digite uma premissa (ou 0 para sair): ").upper()
+
+    premises.append(premise)
+
+conclusion = input("Digite a conclusão: ")
