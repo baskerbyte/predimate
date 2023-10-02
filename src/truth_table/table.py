6f12911e890a2e415a6f7943a25c7d891205f1d4
Luis <baskerbyte@gmail.com>
2023-10-02 07:26:09 -0700

    feat: add truth table

---
+++ src/truth_table/table.py
@@ -0,0 +1,69 @@
+import itertools
+
+from src.entity.operators import *
+
+
+def extract_expressions(expr, expressions=None):
+    if expressions is None:
+        expressions = []
+
+    if isinstance(expr, Negation):
+        extract_expressions(expr.expr, expressions)
+    elif isinstance(expr, Operator):
+        extract_expressions(expr.right, expressions)
+        extract_expressions(expr.left, expressions)
+
+    expressions.append(expr)
+
+    return expressions
+
+
+def order_expressions(expr):
+    extracted_expression = extract_expressions(expr)
+    prepositions = []
+    expressions = []
+
+    for extracted in extracted_expression:
+        if isinstance(extracted, str):
+            prepositions.append(extracted)
+        else:
+            expressions.append(extracted)
+
+    return sorted(prepositions), expressions
+
+
+def generate_combinations(prepositions):
+    return list(itertools.product([True, False], repeat=len(prepositions)))
+
+
+def evaluate_expression(expr, premises, combinations):
+    if isinstance(expr, Operator):
+        left_value = evaluate_expression(expr.left, premises, combinations)
+        right_value = evaluate_expression(expr.right, premises, combinations)
+        result_value = None
+
+        if isinstance(expr, Conjunction):
+            result_value = left_value and right_value
+        elif isinstance(expr, Disjunction):
+            result_value = left_value or right_value
+        elif isinstance(expr, Conditional):
+            result_value = (not left_value) or right_value
+        elif isinstance(expr, BiConditional):
+            result_value = left_value == right_value
+
+        return result_value
+    elif isinstance(expr, str):
+        return combinations[premises.index(expr)]
+
+
+def evaluate_expressions(expressions, combinations):
+    result = []
+
+    for combination in combinations:
+        row = []
+        for expr in expressions[1]:
+            row.append(evaluate_expression(expr, expressions[0], combination))
+
+        result.append(list(combination) + row)
+
+    return result
